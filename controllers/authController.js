df95d2f3db04c95dde2527a91be0f62138ba30bb
Ashi2507 <110546125+Ashi2507@users.noreply.github.com>
2024-09-27 20:52:49 +0530

    nodejs user authentication code changes

---
+++ controllers/authController.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../Models/User');
+
+// @route   POST /api/register
+// @desc    Register user
+// @access  Public
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    // Create new user instance
+    user = new User({ name, email, password });
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save user to the database
+    await user.save();
+
+    // Generate JWT token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 3600 }, // 1 hour
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error type');
+  }
+};
